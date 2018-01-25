b1178f917f4829a05317b329592e548475220c21
jramsdell <ramsdell.jordan@gmail.com>
2018-01-25 12:34:25 -0500

    Initial commit

---
+++ src/main/java/IndexBuilder.java
@@ -0,0 +1,15 @@
+
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.document.*;
+
+public class IndexBuilder {
+
+    IndexBuilder(int start) {
+        start = 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("HI");
+    }
+}
+
