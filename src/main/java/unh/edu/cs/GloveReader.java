b92d8fcab163e15bcad50903d89856e14ebf63c5
jramsdell <ramsdell.jordan@gmail.com>
2018-02-05 19:39:14 -0500

    Added support for reading word vectors and calculating cosine siilarity. Fixed weird dependency with javafx

---
+++ src/main/java/unh/edu/cs/GloveReader.java
@@ -0,0 +1,72 @@
+package unh.edu.cs;
+
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.jooq.lambda.Seq;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.ops.transforms.Transforms;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class GloveReader {
+    final Map<String, INDArray> index;
+
+    private static ImmutablePair<String, INDArray> getEntry(String line) {
+        String[] elements = line.split(" ");
+        double[] result = Arrays.stream(elements)
+                            .skip(1)
+                            .mapToDouble(Double::parseDouble)
+                            .toArray();
+
+        INDArray array = Nd4j.create(result);
+        return new ImmutablePair<String, INDArray>(elements[0], array);
+    }
+
+    GloveReader(String path) throws IOException {
+        index = Seq.seq(Files.lines(Paths.get(path)))
+                .map(GloveReader::getEntry)
+                .toMap(ImmutablePair::getLeft, ImmutablePair::getRight);
+    }
+
+    public INDArray getWordVector(List<String> words) {
+        Double count = 0.0;
+        INDArray array = Nd4j.zeros(50);
+
+        for (String word : words) {
+            INDArray result = index.get(word);
+            if (result != null) {
+                count += 1.0;
+                array.addi(result);
+                System.out.println("THIS");
+                System.out.println(array);
+            }
+        }
+        System.out.println("Final");
+        System.out.println(array);
+        array.muli(2.0);
+        System.out.println(array);
+        return array;
+    }
+
+    public static void main(String[] args) throws IOException {
+        GloveReader gr = new GloveReader("glove.6B.50d.txt");
+        ArrayList<String> ls1 = new ArrayList<>();
+        ls1.add("hi");
+        ls1.add("which");
+        ls1.add("were");
+
+        ArrayList<String> ls2 = new ArrayList<>();
+        ls2.add("hi");
+        ls2.add("which");
+        ls2.add("whatever");
+        ls2.add("industry");
+
+        INDArray id1 = gr.getWordVector(ls1);
+        INDArray id2 = gr.getWordVector(ls2);
+        Double result = Transforms.cosineSim(id1, id2);
+        System.out.println(result);
+    }
+}
+
